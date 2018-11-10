96ab9fd1b0ecd58905e760765f59a5e2fdc7d58f
Saad Malik <saad.t.malik@gmail.com>
2018-11-10 15:26:48 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
   square_array = []
-  array.each do |num|
-    square = num ** 2
-    square_array << square
+  array.each do |num| #you're passing the numbers in the array throgh a block that square's them, adds that to the 'square' variable
+    #and then pushes each of those variales in as elements into an empty array you've defined earlier. 
+    square = num ** 2 #this is how you square a number 
+    square_array << square #this is pushing the elements into an empty array
   end
   square_array
 end
