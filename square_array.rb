77af7962d91056e67c1b23af682de09352e7a590
Saad Malik <saad.t.malik@gmail.com>
2018-11-10 15:27:32 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  square_array = []
+  array.each do |num| #you're passing the numbers in the array throgh a block that square's them, adds that to the 'square' variable
+    #and then pushes each of those variales in as elements into an empty array you've defined earlier.
+    square = num ** 2 #this is how you square a number
+    square_array << square #this is pushing the elements into an empty array
+  end
+  square_array #LASTLY once you've pushed it in, you're displaying the final array, given this is outside of
+  #the end, it would show the array after all the elements in the array have been passed through the block
 end
