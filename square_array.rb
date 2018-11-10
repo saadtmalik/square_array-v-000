4320ded9c3e2afc219e5229eb1a554c4ee8811a5
Saad Malik <saad.t.malik@gmail.com>
2018-11-10 15:22:46 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,6 @@
 def square_array(array)
-  array.each do |square_array, array|
-    array[square_array] = array**2
+  squared_array = []
+  array.each do |square_array|
+    square_array ** 2
   end
 end
